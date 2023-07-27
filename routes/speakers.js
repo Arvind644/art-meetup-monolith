90a5c3a2ae4299d8516fb75b206ce073c2c506f5
arvind singharpuria <arvind.020606@gmail.com>
2023-07-27 18:27:17 +0530

    added base file

---
+++ routes/speakers.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const SpeakerService = require('../services/SpeakerService')
+const router = express.Router()
+
+module.exports = (params) => {
+    const {speakerService} = params
+
+    router.get('/', async (req, res) => {
+        try {
+        const speakers = await speakerService.getList()
+        const artworks = await speakerService.getAllArtwork()  
+        res.render("layout", {template: "speakers", speakers, artworks})
+    }catch(err){
+        return next(err)
+    }
+    })
+
+    router.get('/:shortname', async (req, res) => {
+        try {
+        const speaker = await speakerService.getSpeaker(req.params.shortname)
+        const artworks = await speakerService.getArtworkForSpeaker(req.params.shortname) 
+        res.render("layout", {template: "speaker-detail", speaker, artworks})
+    }catch(err){
+        return next(err)
+    }
+    })
+
+    return router
+}
